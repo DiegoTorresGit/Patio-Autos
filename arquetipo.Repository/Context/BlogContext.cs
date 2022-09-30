f9fc08d7b3be2f3a291aa03b8fb6b8ea2a6cd955
DiegoTorresGit <diegojtorres0@gmail.com>
2022-09-30 16:08:59 -0500

    Add project files.

---
+++ arquetipo.Repository/Context/BlogContext.cs
@@ -0,0 +1,22 @@
+﻿using arquetipo.Entity.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace arquetipo.Repository.Context
+{
+    public partial class BlogContext : DbContext
+    {
+        public BlogContext()
+        {
+        }
+
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Post> Post { get; set; } = null!;
+
+       
+    }
+}
